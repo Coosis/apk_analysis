8b87b1bf9e6100c1f30029f3ddc15483203d423a
Coosis <1159727122@qq.com>
2024-04-20 19:58:17 +0800

    .apk extracting

--- analyse.py
+++ analyse.py
@@ -3,7 +3,7 @@ import os
 
 def find_apk_files():
     """
-    Find all APK files in the .target directory.
+    Find all APK files in the ./target directory.
     rval: list of APK file paths
     """
     root_path = os.path.dirname(os.path.abspath(__file__))
@@ -20,8 +20,20 @@ def find_apk_files():
 
 def main():
     apk_files = find_apk_files()
+    root = os.path.dirname(os.path.abspath(__file__))
     for apk_file in apk_files:
-        apk_extract.disassemble_apk(apk_file)
+        dx = apk_extract.disassemble_apk(apk_file)
+        api_calls = apk_extract.extract_api_calls(dx)
+        apk_name = os.path.basename(apk_file)
+        api_calls_file = os.path.join(root, "output", apk_name, f"{apk_name}_api_calls.txt")
+        print(f"Saving API calls in {api_calls_file}:")
+
+        try:
+            with open(f"{api_calls_file}", "a") as f:
+                for api_call in api_calls:
+                    f.write(f"{api_call}\n")
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
--- apk_extract.py
+++ apk_extract.py
@@ -3,24 +3,53 @@ import os
 from androguard.misc import AnalyzeAPK
 
 def disassemble_apk(apk_path):
+    '''
+    Disassemble APK file and extract AndroidManifest.xml using apktool.
+    :param apk_path: Path to the APK file.
+    :return: Androguard object containing the .dex files.
+    '''
     # APK decoding using apktool
-    path = os.path.dirname(os.path.abspath(__file__))
-    tool_path = os.path.join(path, "target", "apktool")
+    root = os.path.dirname(os.path.abspath(__file__))
+    tool_path = os.path.join(root, "target", "apktool")
     apk_name = os.path.basename(apk_path)
-    output_path = os.path.join(path, "target", "output", apk_name)
+    output_path = os.path.join(root, "output", apk_name)
 
     apktool_command = f"{tool_path} d \"{apk_path}\" -o \"{output_path}\""
-    subprocess.run(apktool_command, shell=True)
+    # subprocess.run(apktool_command, shell=True)
+    process = subprocess.Popen(apktool_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    process.stdin.write('\n')
+    process.stdin.flush()
+
+    _, stderr = process.communicate()
+    # error handling
+
+    if process.returncode != 0:
+        print(f"Error: {stderr}")
+        return None
 
     # Extracting AndroidManifest.xml
-    manifest_path = os.path.join("target", "AndroidManifest.xml")
+    manifest_path = os.path.join(output_path, "AndroidManifest.xml")
     print(f"Extracted AndroidManifest.xml to: {manifest_path}")
 
-    # # Use Androguard to analyze .dex files
-    # a, d, dx = AnalyzeAPK(apk_path)
-    #
-    # # Example: Print all class names
-    # for dex in d:
-    #     for clazz in dex.get_classes():
-    #         print(clazz.get_name())
+    # Use Androguard to analyze .dex files
+    _, _, dx = AnalyzeAPK(apk_path)
 
+    return dx
+
+def extract_api_calls(dx):
+    '''
+    Extract API calls from the .dex files.
+    :param dx: Androguard object containing the .dex files.
+    :return: List of API calls.
+    '''
+    api_calls = []
+    for method in dx.get_external_methods():
+        try:
+            # getting rid of the semicolon
+            class_name = method.class_name[:-1]
+            method_name = method.name
+            api_calls.append(class_name + "/" + method_name)
+        except Exception as e:
+            print(f"Error: {e}")
+
+    return api_calls
